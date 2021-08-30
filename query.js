03cd9ec856e531ced4e4ab4e45c34ea7c936c089
mohsen piri <77721628+Mohsen-graphyy@users.noreply.github.com>
2021-08-31 00:33:44 +0430

    Add files via upload

--- query.js
+++ query.js
@@ -4,3 +4,13 @@ $(window).load(function () {
   // Animate loader off screen
   $(".se-pre-con").fadeOut("slow");
 });
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       // downscroll code
+   } else {
+      // upscroll code
+   }
+   lastScrollTop = st;
+});
